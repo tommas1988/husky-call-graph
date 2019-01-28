af6fb37a89d6d18b9d878eca8053efa2f738cf34
Tommas Yuan <tommas1988@live.com>
2019-01-28 23:00:15 +0800

    test code for class transformation

--- src/main/java/InspectMethodCallClassVisitor.java
+++ src/main/java/InspectMethodCallClassVisitor.java
@@ -1,8 +1,51 @@
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Handle;
 import org.objectweb.asm.MethodVisitor;
 
 public class InspectMethodCallClassVisitor extends ClassVisitor {
     InspectMethodCallClassVisitor(final int api, final ClassVisitor classVisitor) {
         super(api, classVisitor);
+    }
+
+    private static class MethodCallInspector extends MethodVisitor {
+        public MethodCallInspector(final int api, final MethodVisitor methodVisitor) {
+            super(api, methodVisitor);
+        }
+
+        @Override
+        public void visitMethodInsn(
+                final int opcode,
+                final String owner,
+                final String name,
+                final String descriptor,
+                final boolean isInterface) {
+            System.out.println(owner + "::" + name);
+            super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+        }
+
+        @Override
+        public void visitInvokeDynamicInsn(
+                final String name,
+                final String descriptor,
+                final Handle bootstrapMethodHandle,
+                final Object... bootstrapMethodArguments) {
+            System.out.println("Lambda::" + name);
+            super.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, bootstrapMethodArguments);
+        }
+    }
+
+    @Override
+    public MethodVisitor visitMethod(
+            final int access,
+            final String name,
+            final String descriptor,
+            final String signature,
+            final String[] exceptions) {
+        MethodVisitor mv = cv.visitMethod(access, name, descriptor, signature, exceptions);
+        if (mv != null) {
+            return new MethodCallInspector(api, mv);
+        }
+
+        return null;
     }
 }
